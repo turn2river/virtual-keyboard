8585b0a06ac275a58520efed4a8d26e7dd65de2a
alexey.krylov <turn2river@gmail.com>
2023-04-29 23:14:18 +0300

    feat: add design mvp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-const root = document.getElementById('root');
-const main = document.createElement('div');
+// const root = document.getElementById('root');
+// const main = document.createElement('main');
 
-main.innerText = 'welcome';
-root.append(main);
+// main.className = 'main-container main';
+// main.innerText = 'welcome to the AWESOME KEYBOARD';
+// root.append(main);
---
+++ src/data/en.js
@@ -0,0 +1,324 @@
+const en = [
+  {
+    key: '`',
+    shift: '`',
+    code: 'IntlBackslash',
+  },
+  {
+    key: '1',
+    shift: '1',
+    code: 'Digit1',
+  },
+  {
+    key: '2',
+    shift: '2',
+    code: 'Digit2',
+  },
+  {
+    key: '3',
+    shift: '3',
+    code: 'Digit3',
+  },
+  {
+    key: '4',
+    shift: '4',
+    code: 'Digit4',
+  },
+  {
+    key: '5',
+    shift: '5',
+    code: 'Digit5',
+  },
+  {
+    key: '6',
+    shift: '6',
+    code: 'Digit6',
+  },
+  {
+    key: '7',
+    shift: '7',
+    code: 'Digit7',
+  },
+  {
+    key: '8',
+    shift: '8',
+    code: 'Digit8',
+  },
+  {
+    key: '9',
+    shift: '9',
+    code: 'Digit9',
+  },
+  {
+    key: '0',
+    shift: '0',
+    code: 'Digit0',
+  },
+  {
+    key: '-',
+    shift: '-',
+    code: 'Minus',
+  },
+  {
+    key: '=',
+    shift: '=',
+    code: 'Equal',
+  },
+  {
+    key: 'Backspace',
+    shift: 'Backspace',
+    code: 'Backspace',
+  },
+  {
+    key: 'Tab',
+    shift: 'Tab',
+    code: 'Tab',
+  },
+  {
+    key: 'q',
+    shift: 'q',
+    code: 'KeyQ',
+  },
+  {
+    key: 'w',
+    shift: 'w',
+    code: 'KeyW',
+  },
+  {
+    key: 'e',
+    shift: 'e',
+    code: 'KeyE',
+  },
+  {
+    key: 'r',
+    shift: 'r',
+    code: 'KeyR',
+  },
+  {
+    key: 't',
+    shift: 't',
+    code: 'KeyT',
+  },
+  {
+    key: 'y',
+    shift: 'y',
+    code: 'KeyY',
+  },
+  {
+    key: 'u',
+    shift: 'u',
+    code: 'KeyU',
+  },
+  {
+    key: 'i',
+    shift: 'i',
+    code: 'KeyI',
+  },
+  {
+    key: 'o',
+    shift: 'o',
+    code: 'KeyO',
+  },
+  {
+    key: 'p',
+    shift: 'p',
+    code: 'KeyP',
+  },
+  {
+    key: '[',
+    shift: '[',
+    code: 'BracketLeft',
+  },
+  {
+    key: ']',
+    shift: ']',
+    code: 'BracketRight',
+  },
+  {
+    key: '\\',
+    shift: '\\',
+    code: 'Backslash',
+  },
+  {
+    key: 'CapsLock',
+    shift: 'CapsLock',
+    code: 'CapsLock',
+  },
+  {
+    key: 'a',
+    shift: 'a',
+    code: 'KeyA',
+  },
+  {
+    key: 's',
+    shift: 's',
+    code: 'KeyS',
+  },
+  {
+    key: 'd',
+    shift: 'd',
+    code: 'KeyD',
+  },
+  {
+    key: 'f',
+    shift: 'f',
+    code: 'KeyF',
+  },
+  {
+    key: 'g',
+    shift: 'g',
+    code: 'KeyG',
+  },
+  {
+    key: 'h',
+    shift: 'h',
+    code: 'KeyH',
+  },
+  {
+    key: 'j',
+    shift: 'j',
+    code: 'KeyJ',
+  },
+  {
+    key: 'k',
+    shift: 'k',
+    code: 'KeyK',
+  },
+  {
+    key: 'l',
+    shift: 'l',
+    code: 'KeyL',
+  },
+  {
+    key: ';',
+    shift: ';',
+    code: 'Semicolon',
+  },
+  {
+    key: "'",
+    shift: "'",
+    code: 'Quote',
+  },
+  {
+    key: 'Enter',
+    shift: 'Enter',
+    code: 'Enter',
+  },
+  {
+    key: 'Shift',
+    shift: 'Shift',
+    code: 'ShiftLeft',
+  },
+  {
+    key: 'z',
+    shift: 'z',
+    code: 'KeyZ',
+  },
+  {
+    key: 'x',
+    shift: 'x',
+    code: 'KeyX',
+  },
+  {
+    key: 'c',
+    shift: 'c',
+    code: 'KeyC',
+  },
+  {
+    key: 'v',
+    shift: 'v',
+    code: 'KeyV',
+  },
+  {
+    key: 'b',
+    shift: 'b',
+    code: 'KeyB',
+  },
+  {
+    key: 'n',
+    shift: 'n',
+    code: 'KeyN',
+  },
+  {
+    key: 'm',
+    shift: 'm',
+    code: 'KeyM',
+  },
+  {
+    key: ',',
+    shift: ',',
+    code: 'Comma',
+  },
+  {
+    key: '.',
+    shift: '.',
+    code: 'Period',
+  },
+  {
+    key: '/',
+    shift: '/',
+    code: 'Slash',
+  },
+  {
+    key: 'Shift',
+    shift: 'Shift',
+    code: 'ShiftRight',
+  },
+  {
+    key: 'en',
+    shift: '',
+    code: '',
+  },
+  {
+    key: 'Control',
+    shift: 'Control',
+    code: 'ControlLeft',
+  },
+  {
+    key: 'Alt',
+    shift: 'Alt',
+    code: 'AltLeft',
+  },
+  {
+    key: 'Meta',
+    shift: 'Meta',
+    code: 'MetaLeft',
+  },
+  {
+    key: ' ',
+    shift: ' ',
+    code: 'Space',
+  },
+  {
+    key: 'Meta',
+    shift: 'Meta',
+    code: 'MetaRight',
+  },
+  {
+    key: 'Alt',
+    shift: 'Alt',
+    code: 'AltRight',
+  },
+  {
+    key: 'ArrowLeft',
+    shift: 'ArrowLeft',
+    code: 'ArrowLeft',
+  },
+  {
+    key: 'ArrowUp',
+    shift: 'ArrowUp',
+    code: 'ArrowUp',
+  },
+  {
+    key: 'ArrowDown',
+    shift: 'ArrowDown',
+    code: 'ArrowDown',
+  },
+  {
+    key: 'ArrowRight',
+    shift: 'ArrowRight',
+    code: 'ArrowRight',
+  },
+];
+
+export default en;
