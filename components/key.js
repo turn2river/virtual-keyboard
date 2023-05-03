604d079c98166a56d8217ea554e9abf6b066f643
alexey.krylov <turn2river@gmail.com>
2023-05-03 15:04:49 +0300

    feat: deploy

---
+++ components/key.js
@@ -0,0 +1,95 @@
+export default class Key {
+  constructor(key) {
+    this.container = document.createElement('button');
+    this.container.setAttribute('data-code', key.code);
+    this.key = key;
+    this.lang = 'EN';
+    this.container.innerText = this.key.key;
+  }
+
+  render() {
+    switch (this.key.code) {
+      case 'Backspace':
+        this.container.classList.add('button', 'button__right', 'button__bottom', 'button__delete');
+        break;
+      case 'Tab':
+        this.container.classList.add('button', 'button__left', 'button__bottom', 'button__tab');
+        break;
+      case 'CapsLock':
+        this.container.classList.add('button', 'button--special', 'button__left', 'button__bottom', 'button__capslock');
+        break;
+      case 'Enter':
+        this.container.classList.add('button', 'button__right', 'button__bottom', 'button__return');
+        break;
+      case 'ShiftLeft':
+        this.container.classList.add('button', 'button--special', 'button__left', 'button__bottom', 'button__shift');
+        break;
+      case 'ShiftRight':
+        this.container.classList.add('button', 'button--special', 'button__right', 'button__bottom', 'button__shift');
+        break;
+      case 'lang':
+        this.container.classList.add('button', 'button__language');
+        break;
+      case 'ControlLeft':
+        this.container.classList.add('button', 'button--special', 'button__bottom', 'button__control');
+        break;
+      case 'AltLeft':
+        this.container.classList.add('button', 'button--special', 'button__bottom', 'button__alt');
+        break;
+      case 'MetaLeft':
+        this.container.classList.add('button', 'button--special', 'button__bottom', 'button__meta');
+        break;
+      case 'Space':
+        this.container.classList.add('button', 'button__bottom', 'button__space');
+        break;
+      case 'MetaRight':
+        this.container.classList.add('button', 'button--special', 'button__bottom', 'button__meta', 'button__meta--right');
+        break;
+      case 'AltRight':
+        this.container.classList.add('button', 'button--special', 'button__bottom', 'button__alt', 'button__alt--right');
+        break;
+      case 'ArrowLeft':
+        this.container.classList.add('button', 'button-arrow', 'button-arrow__left');
+        break;
+      case 'ArrowUp':
+        this.container.classList.add('button', 'button-arrow', 'button-arrow__up');
+        break;
+      case 'ArrowDown':
+        this.container.classList.add('button', 'button-arrow', 'button-arrow__down');
+        break;
+      case 'ArrowRight':
+        this.container.classList.add('button', 'button-arrow', 'button-arrow__right');
+        break;
+      default:
+        this.container.classList.add('button', 'to-shift');
+    }
+
+    if (this.key.caps) {
+      this.container.classList.add('to-caps');
+    }
+
+    this.container.innerText = this.key.key;
+    return this.container;
+  }
+
+  renderArrows() {
+    switch (this.key.code) {
+      case 'ArrowLeft':
+        this.container.classList.add('button', 'button-arrow', 'button-arrow__left');
+        break;
+      case 'ArrowUp':
+        this.container.classList.add('button', 'button-arrow', 'button-arrow__up');
+        break;
+      case 'ArrowDown':
+        this.container.classList.add('button', 'button-arrow', 'button-arrow__down');
+        break;
+      case 'ArrowRight':
+        this.container.classList.add('button', 'button-arrow', 'button-arrow__right');
+        break;
+      default:
+        this.container.classList.add('button');
+    }
+    this.container.innerText = this.key.key;
+    return this.container;
+  }
+}
